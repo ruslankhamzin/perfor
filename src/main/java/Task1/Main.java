bf05d907dceef5868454d0cefd46800e1ac3354f
Ruslan Khamzin <hamzin362@gmail.com>
2022-04-06 11:25:02 +0400

    0

--- src/main/java/Task1/Main.java
+++ src/main/java/Task1/Main.java
@@ -11,11 +11,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        int c =m;
+        int c = m;
         ArrayList<Integer> list = new ArrayList<Integer>();
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
         List<Integer> result = new ArrayList<>();
         int count=0;
+        int countm=0;
         result.add(1);
         while (true) {
             count++;
@@ -23,14 +23,20 @@ public class Main {
             for (int i = 0; i < n; i++) {
                 num++;
                 list.add(num);
+
             }
-            if(list.get(c)==list.get(0)){
+            if(list.get(c-countm)==1 && c%count==0){
                 break;
             }
-            if(count%(c)==0){
-                result.add(list.get(c));
+            if(count==m && list.get(count)!=1){
+                result.add(list.get(count));
+                countm++;
                 c+=m;
             }
+
+
+
+
 
         }
         for (int i = 0; i < result.size();i++){
